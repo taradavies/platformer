627d9d9a4468a5b3257c212519445fea00c5ac09
Tara Davies <tarawafflebear@gmail.com>
2022-05-30 17:08:31 +0800

    Fixed the characters sprite direction (flip)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 5f;
     Rigidbody2D rb;
     Animator controller;
-    [SerializeField] float moveSpeed = 5f;
+    SpriteRenderer renderer;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         controller = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        // flip only if we're pressing down a key
+        if (horizontalInput != 0)
+            renderer.flipX = horizontalInput < 0;
+
         bool isWalking = horizontalInput!=0;
+        Animate(isWalking);
+    }
+
+    void Animate(bool isWalking) {
         controller.SetBool("isWalking", isWalking);
     }
 }
