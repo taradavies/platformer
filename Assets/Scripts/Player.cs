e2124dd2d86f83ece24c0a34b502db30339cad20
Tara Davies <tarawafflebear@gmail.com>
2022-05-30 17:16:32 +0800

    Added a basic jump.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,19 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 200f;
     Rigidbody2D rb;
     Animator controller;
-    SpriteRenderer renderer;
+    SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         controller = GetComponent<Animator>();
-        renderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -21,10 +23,19 @@ public class Player : MonoBehaviour
 
         // flip only if we're pressing down a key
         if (horizontalInput != 0)
-            renderer.flipX = horizontalInput < 0;
+            spriteRenderer.flipX = horizontalInput < 0;
 
         bool isWalking = horizontalInput!=0;
         Animate(isWalking);
+
+        if (Input.GetButtonDown("Jump")) {
+            ExecuteJump();
+        }
+    }
+
+    private void ExecuteJump()
+    {
+        rb.AddForce(Vector2.up * jumpForce);
     }
 
     void Animate(bool isWalking) {
