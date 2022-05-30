7530edeb860bde61b63ddcffe28222de91e8dc86
Tara Davies <tarawafflebear@gmail.com>
2022-05-30 16:58:53 +0800

    Added a basic walking animation to the player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator controller;
     [SerializeField] float moveSpeed = 5f;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        controller = GetComponent<Animator>();
     }
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        bool isWalking = horizontalInput!=0;
+        controller.SetBool("isWalking", isWalking);
     }
 }
