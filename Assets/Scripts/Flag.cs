6aae3d5d4964170931a641c81dda5063ee67f56c
Tara Davies <tarawafflebear@gmail.com>
2022-05-31 14:41:46 +0800

    Implemented Scene Manager

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Flag : MonoBehaviour
 {
+    [SerializeField] float waitTime = 2f;
     Animator controller;
 
     void Awake() {
@@ -10,6 +12,16 @@ public class Flag : MonoBehaviour
    void OnTriggerEnter2D(Collider2D collider) {
        if (collider.TryGetComponent<Player>(out var player)) {
            controller.SetTrigger("Raise");
+           Invoke("LoadNextLevel", waitTime);
        }
    } 
+
+   void LoadNextLevel() {
+       int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+       if (sceneIndex >= SceneManager.sceneCount) {
+           SceneManager.LoadScene(0);
+           return;
+       }
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+   }
 }
