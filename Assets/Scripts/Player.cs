9d0310dd31ee23173d3d9ece91aecec4cd5f4737
Tara Davies <tarawafflebear@gmail.com>
2022-05-31 08:43:11 +0800

    Added a Ff material on ground & bouncy mushroom

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
 
         // enables friction
 
