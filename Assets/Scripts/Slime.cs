1154fa580d741b4475820f62439647ba5650cc71
Tara Davies <tarawafflebear@gmail.com>
2022-06-01 18:55:02 +0800

    Made the slime a patrol AI

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] Transform frontSensor;
+    [SerializeField] Transform backSensor;
+    [SerializeField] LayerMask mask;
+    [SerializeField] float distanceFromGround;
     Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    float direction;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        direction = -1;
     }
 
     void Update()
     {
-        rb.velocity = Vector2.left;
+        rb.velocity = new Vector2(direction, rb.velocity.y);
+        
+        if (direction < 0)
+        {
+            ScanSensor(frontSensor);
+        }
+        else {
+            ScanSensor(backSensor);
+        }
+    }
+
+    void ScanSensor(Transform sensor)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(
+            sensor.position,
+            Vector2.down,
+            distanceFromGround,
+            mask);
+
+            if (!hit) {
+                TurnAround();
+            }
+    }
+
+    void TurnAround() {
+        direction *= -1;
+        spriteRenderer.flipX = direction > 0;
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
