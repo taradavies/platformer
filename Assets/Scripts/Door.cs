f496d87491a0a1d8899dcca99362702a28eb7992
Tara Davies <tarawafflebear@gmail.com>
2022-06-03 11:14:45 +0800

    Working door with UI that opens up

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -11,20 +11,27 @@ public class Door : MonoBehaviour
     [SerializeField] int requiredCoins = 3;
 
     [SerializeField] Door exitDoor;
+    [SerializeField] Canvas canvas;
+
+    bool isOpen = false;
 
     [ContextMenu("Open Door")]
     void Open() {
+        isOpen = true;
         doorTopRenderer.sprite = doorTop;
         doorMidRenderer.sprite = doorMid;
+        if (canvas!=null) {canvas.enabled = false;}
     }
 
     void Update() {
-        if (Coin.coinsCollected >= requiredCoins && exitDoor!=null) {
+        if (!isOpen && Coin.coinsCollected >= requiredCoins && exitDoor!=null) {
             Open();
         }
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
+        if (!isOpen) {return;}
+
         if (collider.TryGetComponent<Player>(out var player) && exitDoor!=null) {
             player.TeleportTo(exitDoor.transform.position);
         }
