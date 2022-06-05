bf449e3f9d950c6f44c8df48da73193f7b31e878
Tara Davies <tarawafflebear@gmail.com>
2022-06-05 16:05:32 +0800

    2-Way reference with collectibles and collector

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Collectible : MonoBehaviour
 {
+    Collector collector;
     void OnTriggerEnter2D(Collider2D collider) {
         if (collider.TryGetComponent<Player>(out var player)) {
             gameObject.SetActive(false);
+            collector.PickUpCollectible();
         }
+    }
+
+    public void SetCollector(Collector collector) {
+        this.collector = collector;
     }
 }
--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,32 +10,34 @@ public class Collector : MonoBehaviour
 {
     [SerializeField] List<Collectible> collectibles;
     [SerializeField] UnityEvent onCollectionComplete;
+
     TMP_Text remainingCollectibleText;
+    public int countCollected = 0;
 
     void Start() {
         remainingCollectibleText = GetComponentInChildren<TMP_Text>();
+        foreach(var collectible in collectibles) {
+            collectible.SetCollector(this);
+        }
+        remainingCollectibleText?.SetText(collectibles.Count.ToString());
     }
 
 
-    void Update() {
-        int countRemaining = 0;
+    public void PickUpCollectible() {
+        countCollected++;
 
-        foreach(var collectible in collectibles) {
-            if (collectible.isActiveAndEnabled) {
-                countRemaining++;
-            }
-        }
+        int countRemaining = collectibles.Count - countCollected;
 
         remainingCollectibleText?.SetText(countRemaining.ToString());
 
-        if (countRemaining > 0) {return;}
-        else {
+        if (countRemaining <= 0) 
             onCollectionComplete?.Invoke();
-        }
     }
 
     // called when a value is changed within the inspector
     void OnValidate() {
         collectibles = collectibles.Distinct().ToList();
     }
+
+
 }
