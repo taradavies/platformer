3992e3a3b3ba97b4fd633120d8084a6565368630
Tara Davies <tarawafflebear@gmail.com>
2022-06-05 18:07:46 +0800

    Added a coinbox

---
+++ Assets/Scripts/CoinBox.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBox : MonoBehaviour
+{
+    [SerializeField] int totalCoins = 3;
+    int remainingCoins;
+
+    void Start() {
+        remainingCoins = totalCoins;
+    }
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.TryGetComponent<Player>(out var player)) {
+            
+            Vector2 normal = collision.GetContact(0).normal;
+            // If we hit the object from above increment the coins collected
+            if (normal.y < 0 && remainingCoins > 0) {
+                Coin.coinsCollected++;
+                remainingCoins--;  
+            }
+
+        }
+    }
+}
