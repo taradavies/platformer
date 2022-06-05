2d92c1e1ce04a73de576b375e299f21e57cbe3d1
Tara Davies <tarawafflebear@gmail.com>
2022-06-05 11:34:29 +0800

    Added UI elements to the collector

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
     [SerializeField] Collectible[] collectibles;
+    TMP_Text remainingCollectibleText;
+
+    void Start() {
+        remainingCollectibleText = GetComponentInChildren<TMP_Text>();
+    }
 
 
     void Update() {
-        // if any items are still active
+        int countRemaining = 0;
+        
         foreach(var collectible in collectibles) {
             if (collectible.isActiveAndEnabled) {
-                return;
+                countRemaining++;
             }
         }
-        // if (collectibles.Any(t => t.gameObject.activeSelf))
-        //     return;
+        
+        // if its not null --> '?'
+        remainingCollectibleText?.SetText(countRemaining.ToString());
+
+        if (countRemaining > 0) {return;}
     }
 
 
