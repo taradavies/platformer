a6199e2854f8507a73188bb14d3148f1bff0f27d
Tara Davies <tarawafflebear@gmail.com>
2022-06-07 14:40:21 +0800

    Added a PLAY button

---
+++ Assets/Scripts/UIStartLevelButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIStartLevelButton : MonoBehaviour
+{
+    public void LoadLevel() {
+        SceneManager.LoadScene(1);
+    }
+}
