30b50cc87e9e26316f8cd9b941bdd5a57a727483
Tara Davies <tarawafflebear@gmail.com>
2022-06-08 16:22:13 +0800

    Added music and singleton pattern

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public static Music Instance { get; private set; }
+    void Awake()
+    {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+    
+}
